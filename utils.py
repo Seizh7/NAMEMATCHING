084d4f2006abba68773218249bd3fb415ca945d9
Seizh7 <social_seizh@protonmail.com>
2025-07-28 22:07:54 +0200

    Add save_json function

--- utils.py
+++ utils.py
@@ -1,9 +1,18 @@
 import json
+import re
+import os
 
 
 def load_json(path):
-    with open(path, "r", encoding="utf-8") as f:
-        return json.load(f)
+    if os.path.exists(path):
+        with open(path, "r", encoding="utf-8") as f:
+            return json.load(f)
+    return {}
+
+
+def save_json(path, data):
+    with open(path, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
 
 
 def normalize_name(name):
