4ce053d6ed092e4d9e243561d88aba6464f442b0
Seizh7 <social_seizh@protonmail.com>
2025-08-03 11:16:36 +0200

    add main to metrics module

---
+++ namematching/metrics/main.py
@@ -0,0 +1,13 @@
+from namematching.metrics.extract_features import extract_features
+
+
+def main():
+    extract_features(
+        input_path="data/pairs_generated.csv",
+        output_path="data/pairs_and_features.csv",
+        batch=5000
+    )
+
+
+if __name__ == "__main__":
+    main()
