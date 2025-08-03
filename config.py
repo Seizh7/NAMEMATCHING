ba1c8e6142f7f221054a9a6fbb898ce8d4b1ffd1
Seizh7 <social_seizh@protonmail.com>
2025-08-03 17:35:48 +0200

    Add config.py file for constants

---
+++ config.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+
+class Config:
+    def __init__(self):
+        # Root of the project
+        self.project_root = Path(__file__).resolve().parent
+
+        # Data and results directories
+        self.data_dir = self.project_root / "data"
+        self.export_dir = self.project_root / "export"
+        self.source_dir = self.project_root / "namematching"
+        self.scraping_dir = self.source_dir / "scraping"
+        self.databuild_dir = self.source_dir / "databuild"
+        self.metrics_dir = self.source_dir / "metrics"
+        self.model_dir = self.source_dir / "model"
+
+
+CONFIG = Config()
