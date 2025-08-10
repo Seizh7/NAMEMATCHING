a81c1a1e79629d7088256873c80daf80136bcfe6
Seizh7 <social_seizh@protonmail.com>
2025-08-10 10:17:37 +0200

    change argument's name

--- namematching/metrics/main.py
+++ namematching/metrics/main.py
@@ -5,7 +5,7 @@ def main():
     extract_features(
         input_path="data/pairs_generated.csv",
         output_path="data/pairs_and_features.csv",
-        batch=5000
+        batch_size=5000
     )
 
 
